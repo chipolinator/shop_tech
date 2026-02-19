1d26c814fdf0c90d04a0293536067355ca57a6be
Alexander Titilin <lambdadelta89@gmail.com>
2026-02-19 12:55:45 +0300

    fix hash

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,14 +1,13 @@
-# from passlib.context import CryptContext
-# pwd_context = CryptContext(
-#     schemes=["bcrypt"],
-#     deprecated="auto",
-#     bcrypt__rounds=12
-# )
-#
-#
+from passlib.context import CryptContext
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+)
+
+
 def hash_password(password: str) -> str:
-    return str(hash(password))
+    return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return hash_password(password) == hashed_password
+    return pwd_context.verify(plain_password, hash_password)
