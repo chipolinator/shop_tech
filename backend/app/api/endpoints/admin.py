1f087d0ffb42e1c0f1e0f6c4eca893463d3d4470
Alexander Titilin <lambdadelta89@gmail.com>
2026-02-20 16:17:21 +0300

    add create add

--- backend/app/api/endpoints/admin.py
+++ backend/app/api/endpoints/admin.py
@@ -1,13 +1,30 @@
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile, Form
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from utils.security import verify_password,create_access_token,decode_token
+from utils.security import verify_password, create_access_token, decode_token
 from config import settings
 from datetime import timedelta
-
+from schemas.car import DriveType, Car
+from pathlib import Path
+from database.database import add_car_db
+from PIL import Image
+import io
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/admin/token")
 router = APIRouter()
+
+
+async def get_admin(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    username = decode_token(token)
+    admin = settings.ADMIN_NAME
+    if admin is None:
+        raise credentials_exception
+    return admin
 
 
 @router.post("/token", status_code=200)
@@ -20,3 +37,50 @@ async def token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         expires_delta=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/create_car", status_code=200)
+async def create_car(admin: Annotated[settings.ADMIN_NAME, Depends(get_admin)],
+                     brand: str = Form(...),
+                     model: str = Form(...),
+                     power: int = Form(...),
+                     displacement: float = Form(...),
+                     drive: DriveType = Form(...),
+                     price: float = Form(...),
+                     image: UploadFile = File(...)):
+    allowed_formats = {'JPEG', 'PNG'}
+    try:
+        contents = await image.read()
+        img = Image.open(io.BytesIO(contents))
+        img.verify()
+
+        if img.format not in allowed_formats:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Only PNG и JPEG! Get {img.format}"
+            )
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=400,
+            detail="File Error!"
+        )
+
+    Path("uploads/cars").mkdir(parents=True, exist_ok=True)
+    file_path = f"uploads/cars/{image.filename}"
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(contents)
+
+    db_car = Car(
+        brand=brand,
+        model=model,
+        power=power,
+        displacement=displacement,
+        drive=DriveType(drive),
+        price=price,
+        image_path=file_path
+    )
+    add_car_db(db_car)
+    return db_car
