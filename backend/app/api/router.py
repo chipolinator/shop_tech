1a961695726626a628fb5233a5efcf41121a27a9
Alexander Titilin <lambdadelta89@gmail.com>
2026-02-20 16:30:31 +0300

    add get all cars

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .endpoints import login,admin
+from .endpoints import login,admin,cars
 
 router = APIRouter()
 
 router.include_router(login.router, prefix="/reg", tags=["reg"])
 router.include_router(admin.router, prefix="/admin", tags=["admin"])
+router.include_router(cars.router, prefix="/cars", tags=["cars"])
