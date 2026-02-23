ced654ff0d3387184cb1c241593d9e36b71aa672
Alexander Titilin <lambdadelta89@gmail.com>
2026-02-23 11:31:11 +0300

    fix car load

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,25 +12,14 @@ app = FastAPI(
     title=settings.APP_NAME
 )
 
-ALLOWED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
-    "http://localhost:80",
-    "http://127.0.0.1:80",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 @app.on_event("startup")
